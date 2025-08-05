6f8de378769cbeb3537753b8d5a50d70f6a5d79d
Howboi <howard0427666@gmail.com>
2025-08-05 18:51:11 +0800

    .

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-    base: '/project/',
+export default defineConfig(({ command }) =>({
+    base: command === 'serve' ? '/' : '/project/',
     host: '0.0.0.0',
     server: {
       allowedHosts:['pan.yuan36.xyz'],
@@ -13,3 +13,4 @@ export default defineConfig({
       vue(),
       tailwindcss()],
 })
+)
